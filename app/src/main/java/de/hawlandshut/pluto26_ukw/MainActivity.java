23eeb42eebaa7275ac10890e8c1c5d38d9457f54
Prof-Greipl <dieter.greipl@gmail.com>
2025-11-09 11:46:42 +0100

    T2 (am 04.11.2025)

--- app/src/main/java/de/hawlandshut/pluto26_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto26_ukw/MainActivity.java
@@ -1,14 +1,23 @@
 package de.hawlandshut.pluto26_ukw;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "xxx MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // TODO: Nur zum Test, später löschen
+        Log.d(TAG, "onCreate");
+
+        // TODO: Toast zum Testen
+        Toast.makeText(getApplicationContext(), "App started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_main_manageaccount) {
+            Toast.makeText(getApplicationContext(), "You pressed ManageAccount", Toast.LENGTH_LONG).show();
+        }
+
+        if (item.getItemId() == R.id.menu_main_help) {
+            Toast.makeText(getApplicationContext(), "You pressed Help", Toast.LENGTH_LONG).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
     }
 }
